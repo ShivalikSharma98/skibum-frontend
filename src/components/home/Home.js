7bde8f86ecc495a5724a4621cc0c52c8e9ca29ff
ShivalikSharma98 <Shivaliksharma98@gmail.com>
2022-07-29 21:29:29 -0600

    styles for home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,37 @@
+import './Home.css';
 import React from 'react';
-import Header from '../headers/header/Header'
+import { Link } from 'react-router-dom';
+import Header from '../headers/header/Header';
 
 function Home() {
-    return (
-        <div className='home'>
-            <Header />
-            <h1>This is the home page</h1>
-        </div>
-    );
+	return (
+		<div className='home'>
+			<Header />
+			<div className='home-items-container'>
+				<h1 className='title'>Ski Bum</h1>
+				<div className='resorts-item'>
+					<Link to='/resorts'>
+						<button>Resorts</button>
+					</Link>
+				</div>
+				<div className='weather-item'>
+					<Link to='/weather'>
+						<button>Weather</button>
+					</Link>
+				</div>
+				<div className='gallery-item'>
+					<Link to='/gallery'>
+						<button>Gallery</button>
+					</Link>
+				</div>
+				<div className='forum-item'>
+					<Link to='/posts'>
+						<button>Backcountry Forum</button>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
