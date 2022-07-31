0ce180910ac7169630b92a63f8042369bf5231c0
ShivalikSharma98 <Shivaliksharma98@gmail.com>
2022-07-30 20:46:00 -0600

    axios get for all posts working

--- src/components/forum/posts/Posts.js
+++ src/components/forum/posts/Posts.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Posts.css'
+import Header from '../../headers/header/Header';
+import { config } from '../../../Constants';
+import axios from 'axios';
 
 function Posts() {
+	const [posts, setPosts] = useState(null);
+
+	function getPosts() {
+		const url = `${config.API_URL}/api/posts`;
+		axios
+			.get(url)
+			.then((res) => {
+				setPosts(res.data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
+	useEffect(() => {
+		getPosts();
+	}, []);
+
 	return (
-		<div>
-			<h1>This is the Posts Page</h1>
+		<div className='posts'>
+			<Header />
+			<div className='posts-container'>
+				
+			</div>
 		</div>
 	);
 }
--- src/components/resorts/Resorts.js
+++ src/components/resorts/Resorts.js
@@ -3,7 +3,7 @@ import Header from '../headers/header/Header';
 
 function Resorts() {
     return (
-        <div className='resots'>
+        <div className='resorts'>
             <Header />
             <h1>This is the resorts page</h1>
         </div>
