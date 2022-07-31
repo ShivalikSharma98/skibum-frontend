4f461978935423248f4c4f9e807f5e4c9a4bb38e
ShivalikSharma98 <Shivaliksharma98@gmail.com>
2022-07-31 02:43:10 -0600

    seeded data and fetched on frontend displayed into cards

--- src/components/forum/posts/Posts.js
+++ src/components/forum/posts/Posts.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import './Posts.css'
+import './Posts.css';
 import Header from '../../headers/header/Header';
 import { config } from '../../../Constants';
 import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function Posts() {
-	const [posts, setPosts] = useState(null);
+	const [posts, setPosts] = useState([]);
 
 	function getPosts() {
 		const url = `${config.API_URL}/api/posts`;
@@ -22,11 +24,37 @@ function Posts() {
 		getPosts();
 	}, []);
 
+	if (!posts.length) {
+		return (
+			<div className='posts'>	
+				<Header />
+				<h3>There are currently no posts on this forum</h3>
+			</div>
+		);
+	}
+
 	return (
 		<div className='posts'>
 			<Header />
 			<div className='posts-container'>
-				
+				{posts.map((post) => (
+					<Card className='forum-cards' key={post.id}>
+						<Card.Header className='forum-cards-header'>
+							<h3>{post.title}</h3>
+						</Card.Header>
+						<Card.Body>
+							<Card.Title>
+								<h4>By: {post.username}</h4>
+							</Card.Title>
+							<h6>Difficulty: {post.difficulty}</h6>
+							<h6>Start Location: {post.start_location}</h6>
+							<h6>End Location: {post.end_location}</h6>
+							<p>Directions: {post.directions}</p>
+							<p>{post.description}</p>
+						</Card.Body>
+						<Card.Img src={post.image_url} />
+					</Card>
+				))}
 			</div>
 		</div>
 	);
