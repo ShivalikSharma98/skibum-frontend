2ec27faaf187dced3c2253b3979e700b141a8c2a
ShivalikSharma98 <Shivaliksharma98@gmail.com>
2022-07-31 16:55:54 -0600

    login and signup forms

--- src/components/forum/login/Login.js
+++ src/components/forum/login/Login.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from '../../headers/header/Header';
+import './Login.css';
+import { Link } from 'react-router-dom';
 
 function Login() {
+	const [username, setUsername] = useState(null);
 	return (
-		<div className='login'>
-			<h1>This is the Login page</h1>
-		</div>
-	)
+		<>
+			<Header />
+			<div className='login'>
+				<div className='login-form'>
+					<form>
+						<h2 className='text-center'>Log in</h2>
+						<div className='form-group'>
+							<input
+								id='username'
+								type='text'
+								className='form-control'
+								placeholder='Username'
+								required='required'
+							/>
+						</div>
+						<div className='form-group'>
+							<input
+								id='password'
+								type='password'
+								className='form-control'
+								placeholder='Password'
+								required='required'
+							/>
+						</div>
+						<div className='form-group'>
+							<button type='submit' className='btn btn-primary btn-block'>
+								Log in
+							</button>
+						</div>
+					</form>
+					<p className='text-center'>
+						<Link to='/posts/signup'>Create an Account</Link>
+					</p>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Login;
--- src/components/forum/signup/SignUp.js
+++ src/components/forum/signup/SignUp.js
@@ -1,7 +1,60 @@
 import React from 'react';
+import Header from '../../headers/header/Header';
 
 function SignUp() {
-	return <div></div>;
+	return (
+		<>
+			<Header />
+			<div className='login'>
+				<div className='login-form'>
+					<form>
+						<h2 className='text-center'>Sign up</h2>
+						<div className='form-group'>
+							<input
+								id='name'
+								type='text'
+								className='form-control'
+								placeholder='Name'
+								required='required'
+							/>
+						</div>
+						<div className='form-group'>
+							<input
+								id='email'
+								type='text'
+								className='form-control'
+								placeholder='Email'
+								required='required'
+							/>
+						</div>
+						<div className='form-group'>
+							<input
+								id='username'
+								type='text'
+								className='form-control'
+								placeholder='Username'
+								required='required'
+							/>
+						</div>
+						<div className='form-group'>
+							<input
+								id='password'
+								type='password'
+								className='form-control'
+								placeholder='Password'
+								required='required'
+							/>
+						</div>
+						<div className='form-group'>
+							<button type='submit' className='btn btn-primary btn-block'>
+								Sign up
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default SignUp;
