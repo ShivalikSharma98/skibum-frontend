1384af6b2c539c660e8604251d191a7ae16231f8
ShivalikSharma98 <Shivaliksharma98@gmail.com>
2022-07-31 03:00:43 -0600

    forum styles

--- src/components/forum/posts/Posts.js
+++ src/components/forum/posts/Posts.js
@@ -3,8 +3,8 @@ import './Posts.css';
 import Header from '../../headers/header/Header';
 import { config } from '../../../Constants';
 import axios from 'axios';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function Posts() {
 	const [posts, setPosts] = useState([]);
@@ -26,7 +26,7 @@ function Posts() {
 
 	if (!posts.length) {
 		return (
-			<div className='posts'>	
+			<div className='posts'>
 				<Header />
 				<h3>There are currently no posts on this forum</h3>
 			</div>
@@ -34,29 +34,35 @@ function Posts() {
 	}
 
 	return (
-		<div className='posts'>
+		<>
 			<Header />
-			<div className='posts-container'>
-				{posts.map((post) => (
-					<Card className='forum-cards' key={post.id}>
-						<Card.Header className='forum-cards-header'>
-							<h3>{post.title}</h3>
-						</Card.Header>
-						<Card.Body>
-							<Card.Title>
-								<h4>By: {post.username}</h4>
-							</Card.Title>
-							<h6>Difficulty: {post.difficulty}</h6>
-							<h6>Start Location: {post.start_location}</h6>
-							<h6>End Location: {post.end_location}</h6>
-							<p>Directions: {post.directions}</p>
-							<p>{post.description}</p>
-						</Card.Body>
-						<Card.Img src={post.image_url} />
-					</Card>
-				))}
+			<div className='posts'>
+				<LinkContainer to='/posts/login'>
+					<button className='button-49'>Login</button>
+				</LinkContainer>
+				<div className='posts-container'>
+					<h1>Forum</h1>
+					{posts.map((post) => (
+						<Card className='forum-cards' key={post.id}>
+							<Card.Header className='forum-cards-header'>
+								<h3>{post.title}</h3>
+							</Card.Header>
+							<Card.Body>
+								<Card.Title>
+									<h4>By: {post.username}</h4>
+								</Card.Title>
+								<h6>Difficulty: {post.difficulty}</h6>
+								<h6>Start Location: {post.start_location}</h6>
+								<h6>End Location: {post.end_location}</h6>
+								<p>Directions: {post.directions}</p>
+								<p>{post.description}</p>
+							</Card.Body>
+							<Card.Img src={post.image_url} />
+						</Card>
+					))}
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
