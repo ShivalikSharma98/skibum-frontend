575bb095a82798506fafa612cc65f0518ea1d373
ShivalikSharma98 <Shivaliksharma98@gmail.com>
2022-08-03 04:12:43 -0600

    about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import Header from '../headers/header/Header';
+import { Card } from 'react-bootstrap';
 
 function About() {
 	return (
-		<div className='gallery'>
+		<>
 			<Header />
-			<h1>This is the Gallery page</h1>
-		</div>
+			<div className='posts'>
+				<div className='login-form'>
+					<Card className='forum-cards'>
+						<Card.Header className='forum-cards-header'>
+							<h4>About</h4>
+						</Card.Header>
+						<Card.Body>
+							Ski Bum is a full stack web application designed for Skiers and
+							Snowboarders in Colorado. Users can visit the site to find useful
+							information on resorts and even access information provided by
+							other users on secret backcountry locations.
+						</Card.Body>
+						<Card.Img
+							style={{ 'max-width': '30rem' }}
+							src='https://repository-images.githubusercontent.com/248812720/56902700-c5bd-11ea-813f-ed8631377258'
+						/>
+					</Card>
+				</div>
+			</div>
+		</>
 	);
 }
 
